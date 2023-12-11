350486d488b503502eae1e2878171ff3e15901a1
Dargonit <igrokyse@gmail.com>
2023-12-11 14:21:40 -0800

    added selectors for game elements added images for selections added one round game mechanics

--- scripts/game.js
+++ scripts/game.js
@@ -8,43 +8,53 @@ function getComputerChoice(){
     return "Scissors";
 }
 
-function capitalize(str){
-    const firstLetter = str.charAt(0).toUpperCase();
-    return  firstLetter + str.slice(1).toLowerCase();
-}
-
 function playRound(playerSelection, computerSelection) {
+    let gameOutput = document.getElementById("game-output");
     while (playerSelection == computerSelection){
         computerSelection = getComputerChoice();
     }
     if(playerSelection == "Rock" && computerSelection == "Paper"){
-        console.log( "You Lose! " + computerSelection +" beats " + playerSelection);
+        gameOutput.textContent ="You Lose! " + computerSelection +" beats " + playerSelection;
         return 0;
     }else if(playerSelection == "Rock" && computerSelection == "Scissors"){
-        console.log( "You Win! " + playerSelection +" beats " + computerSelection);
+        gameOutput.textContent = "You Win! " + playerSelection +" beats " + computerSelection;
         return 1;
     }else if(playerSelection == "Paper" && computerSelection == "Scissors"){
-        console.log( "You Lose! " + computerSelection +" beats " + playerSelection);
+        gameOutput.textContent = "You Lose! " + computerSelection +" beats " + playerSelection;
         return 0;
     }else if(playerSelection == "Paper" && computerSelection == "Rock"){
-        console.log("You Win! " + playerSelection +" beats " + computerSelection);
+        gameOutput.textContent = "You Win! " + playerSelection +" beats " + computerSelection;
         return 1;
     }else if(playerSelection == "Scissors" && computerSelection == "Rock"){
-        console.log( "You Lose! "+ computerSelection +" beats " + playerSelection);
+        gameOutput.textContent = "You Lose! "+ computerSelection +" beats " + playerSelection;
         return 0;
     }else{
-        console.log( "You Win! "+ playerSelection +" beats " + computerSelection);
+        gameOutput.textContent = "You Win! "+ playerSelection +" beats " + computerSelection;
         return 1;
     }
   }
    
   function game(){
     let gamesWon = 0;
-    for(let i=0;i<5; i++){
-        const playerSelection = capitalize(prompt("Rock,Paper,Scissors?"));
-        const computerSelection = getComputerChoice();
-        gamesWon += playRound(playerSelection, computerSelection);
-    }
-
     console.log("You won " + gamesWon + " games!!!");
+  }
+
+
+  window.onload = function(){
+    let selectionRock = document.getElementById("rock")
+    selectionRock.addEventListener("click", function(){
+        playRound("Rock",getComputerChoice());
+    });
+
+    let selectionPaper = document.getElementById("paper")
+    selectionPaper.addEventListener("click", function(){
+        playRound("Paper",getComputerChoice());
+    });
+
+    let selectionScissors = document.getElementById("scissors")
+    selectionScissors.addEventListener("click", function(){
+        playRound("Scissors",getComputerChoice());
+    });
+
+
   }
