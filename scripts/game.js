9516f0ff97af2425bb4fdc19b45a909962868692
Dargonit <igrokyse@gmail.com>
2023-12-16 15:57:33 -0800

    updated styling added new game logic added round count added rules description

--- scripts/game.js
+++ scripts/game.js
@@ -15,32 +15,53 @@ function playRound(playerSelection, computerSelection) {
     }
     if(playerSelection == "Rock" && computerSelection == "Paper"){
         gameOutput.textContent ="You Lose! " + computerSelection +" beats " + playerSelection;
-        return 0;
+        updateScore(0);
     }else if(playerSelection == "Rock" && computerSelection == "Scissors"){
         gameOutput.textContent = "You Win! " + playerSelection +" beats " + computerSelection;
-        return 1;
+        updateScore(1);
     }else if(playerSelection == "Paper" && computerSelection == "Scissors"){
         gameOutput.textContent = "You Lose! " + computerSelection +" beats " + playerSelection;
-        return 0;
+        updateScore(0);
     }else if(playerSelection == "Paper" && computerSelection == "Rock"){
         gameOutput.textContent = "You Win! " + playerSelection +" beats " + computerSelection;
-        return 1;
+        updateScore(1);
     }else if(playerSelection == "Scissors" && computerSelection == "Rock"){
         gameOutput.textContent = "You Lose! "+ computerSelection +" beats " + playerSelection;
-        return 0;
+        updateScore(0);
     }else{
         gameOutput.textContent = "You Win! "+ playerSelection +" beats " + computerSelection;
-        return 1;
+        updateScore(1);
     }
   }
-   
-  function game(){
-    let gamesWon = 0;
-    console.log("You won " + gamesWon + " games!!!");
-  }
+
+function updateScore(score){
+    let gameScore = document.getElementById("game-score");
+    window.gamesPlayed +=1;
+    if(score == -1){
+        gameScore.textContent = "0";
+        window.gamesPlayed =0;
+    }
+    else if(window.gamesPlayed >= 5){
+        if(parseInt(gameScore.textContent) > 2){
+            alert("Congratulations, You won!!");
+        }else{
+            alert("Sorry, You lose!!");
+        }
+        window.gamesPlayed =0;
+        gameScore.textContent = "0";
+    }
+    else{
+        gameScore.textContent = parseInt(gameScore.textContent) + score;
+    }
+}
 
 
-  window.onload = function(){
+window.onload = function(){
+    window.gamesPlayed =0;
+    let newGame = document.getElementById("new-game")
+    newGame.addEventListener("click", function(){
+        updateScore(-1);
+    });
     let selectionRock = document.getElementById("rock")
     selectionRock.addEventListener("click", function(){
         playRound("Rock",getComputerChoice());
